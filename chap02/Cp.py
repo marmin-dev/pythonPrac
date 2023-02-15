b3d1168213e0a8d1df91681212d38fa2d65e8fca
Marmin <marmin.dev@gmail.com>
2023-02-15 17:25:07 +0900

    chap 04 end studying about comparison operator

---
+++ chap02/Cp.py
@@ -0,0 +1,11 @@
+print(2 == 2)
+print(2 == 1)
+print('hello' == "hello")
+print('2' == 2)
+print('2' != 2)
+
+print(1<2<3)
+print(1 < 2 and 2 < 3)
+print(1 < 2 or 2 < 3)
+print(1 < 2 ,not 2 < 3)
+# not returns the opposite result of boolean
